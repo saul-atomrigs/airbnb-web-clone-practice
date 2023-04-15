81258cdecd75a92996579956387840e36314c1ca
Saul Lee <solleedata@gmail.com>
2023-04-15 22:16:20 +0900

    NavBar UI

---
+++ app/components/Container.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+type ChildrenType = {
+  children: React.ReactNode;
+};
+
+export default function Container({ children }: ChildrenType) {
+  return (
+    <div
+      className="
+        max-w-[2520px]
+        mx-auto
+        xl:px-20 
+        md:px-10
+        sm:px-2
+        px-4
+      "
+    >
+      {children}
+    </div>
+  );
+}
