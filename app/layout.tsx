ad1f9b5daaa71808173394b5319639c29607edcd
Saul Lee <solleedata@gmail.com>
2023-04-19 12:51:11 +0900

    Auth UI - ClientOnly

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import ClientOnly from "./components/ClientOnly";
 import "./globals.css";
 import { Nunito } from "next/font/google";
 
@@ -17,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <ClientOnly>{children}</ClientOnly>
+      </body>
     </html>
   );
 }
