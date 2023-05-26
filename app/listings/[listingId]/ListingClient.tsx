465fb267a2c0b5390262c487c01c9896eb4dff4b
Saul Lee <saul@atomrigs.io>
2023-05-26 13:06:49 +0900

    ListingHead 컴포넌트

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,4 +1,5 @@
 import Container from "@/app/components/Container";
+import ListingHead from "@/app/components/listings/ListingHead";
 import { categories } from "@/app/components/navbar/Categories";
 import useLoginModal from "@/app/hooks/useLoginModal";
 import { SafeListing, SafeReservation, SafeUser } from "@/app/types";
